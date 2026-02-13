cdbcbe0a63e399a8291ac3c04cfbc20ec187d412
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-02-12 23:25:50 -0300

    alterar popup ao clicar no usuário

--- src/components/custom/sidebar-user.tsx
+++ src/components/custom/sidebar-user.tsx
@@ -1,7 +1,9 @@
-import { CircleUserRoundIcon, EllipsisVerticalIcon, LogOutIcon } from 'lucide-react'
-import { Skeleton } from '../ui/skeleton'
+import packageJson from "../../../package.json";
 
-import cobeb_profile_light from '@/assets/cobeb-profile-light.png'
+import { EllipsisVerticalIcon, LogOutIcon, MoonIcon } from 'lucide-react';
+import { Skeleton } from '../ui/skeleton';
+
+import cobeb_profile_light from '@/assets/cobeb-profile-light.png';
 
 import {
   DropdownMenu,
@@ -9,23 +11,27 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+} from '@/components/ui/dropdown-menu';
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from '@/components/ui/sidebar'
-import { useAuth } from '@/hooks/use-auth'
+} from '@/components/ui/sidebar';
+import { useAuth } from '@/hooks/use-auth';
 
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
-import { formatCPF } from '@/utils/formatters'
+import { formatCPF } from '@/utils/formatters';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { Theme, useTheme } from "@/hooks/use-theme";
 
 export function SidebarUser() {
   const { isMobile } = useSidebar()
   const { user, signOut, loading } = useAuth()
+  const { theme, setTheme } = useTheme();
 
   return (
     <SidebarMenu>
@@ -62,35 +68,34 @@ export function SidebarUser() {
             sideOffset={4}
           >
             <DropdownMenuLabel className='p-0 font-normal'>
-              <div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm'>
-                <Avatar>
-                  <AvatarImage src={cobeb_profile_light} />
-                  <AvatarFallback>{user?.nome.charAt(0).concat(user?.nome.charAt(1) || '').toUpperCase()}</AvatarFallback>
-                </Avatar>
-                <div className='grid flex-1 text-left text-sm leading-tight'>
-                  {loading ? (
-                    <Skeleton className='p-2 mb-1 w-22' />
-                  ) : (
-                    <span className='truncate font-medium'>{user?.nome.toUpperCase()}</span>
-                  )}
-                  {loading ? (
-                    <Skeleton className='p-1' />
-                  ) : (
-                    <span className='text-muted-foreground truncate text-xs'>{formatCPF(user?.cpf || '')}</span>
-                  )}
-                </div>
-              </div>
+              <DropdownMenuGroup>
+                <DropdownMenuItem disabled>Versão atual: {packageJson.version}</DropdownMenuItem>
+              </DropdownMenuGroup>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem className='gap-2'>
-                <CircleUserRoundIcon size={18}/>
-                Conta
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <DropdownMenuItem className='gap-2'>
+                  <MoonIcon size={18} />
+                  Tema
+                </DropdownMenuItem>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent side="right" className="w-56 ml-1 mb-12">
+                <DropdownMenuLabel>Escolha o tema de sua preferência!</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuRadioGroup value={theme} onValueChange={(value) => setTheme(value as Theme)}>
+                  <DropdownMenuRadioItem value="light">Claro</DropdownMenuRadioItem>
+
+                  <DropdownMenuRadioItem value="dark">Escuro</DropdownMenuRadioItem>
+
+                  <DropdownMenuRadioItem value="system">Automático</DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
             <DropdownMenuSeparator />
             <DropdownMenuItem onClick={signOut} className='gap-2'>
-              <LogOutIcon size={18}/>
+              <LogOutIcon size={18} />
               Sair
             </DropdownMenuItem>
           </DropdownMenuContent>
