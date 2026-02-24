d78901b7675cf94937de624d8200372cbd485431
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-02-24 17:06:11 -0300

    integrar capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gestao_reposicao.cobeb',
+  appName: 'Cobeb | Gestão de Reposição',
+  webDir: 'dist'
+};
+
+export default config;
