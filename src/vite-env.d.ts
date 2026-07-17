c5bddef018f94e07977f9348055b5821449f8d2d
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-07-17 01:17:44 -0300

    refactor: refatorar tipagem das variáveis de ambiente disponíveis

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 interface ImportMetaEnv {
-  readonly VITE_APP_VERSION: string
-  readonly VITE_APP_URL: string
+  readonly VITE_API_URL: string
   readonly VITE_APP_MODE: 'development' | 'production'
-
-  readonly VITE_GOOGLE_CLIENT_ID: string
 
   readonly VITE_REVERB_APP_KEY: string
   readonly VITE_REVERB_HOST: string
