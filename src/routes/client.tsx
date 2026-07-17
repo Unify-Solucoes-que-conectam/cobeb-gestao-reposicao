1b1a01909cc7adb75d6cd7c55dca5f22530826d7
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-07-17 00:50:07 -0300

    feat: criar rota /registrar-avarias

--- src/routes/client.tsx
+++ src/routes/client.tsx
@@ -1,7 +1,8 @@
 import type { RouteObject } from 'react-router-dom'
 
-import ClientHome from '@/pages/client/home'
 import ClientAvariasRegistradas from '@/pages/client/avarias-registradas'
+import ClientHome from '@/pages/client/home'
+import ClientRegistrarAvarias from '@/pages/client/registrar-avarias'
 
 export const clientRoutes: RouteObject[] = [
   {
@@ -11,5 +12,9 @@ export const clientRoutes: RouteObject[] = [
   {
     path: 'avarias-registradas',
     element: <ClientAvariasRegistradas />,
+  },
+  {
+    path: 'registrar-avarias',
+    element: <ClientRegistrarAvarias />,
   },
 ]
