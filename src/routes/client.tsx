3affa40087a0f18b75ad413f49ebe3f9908421a4
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-07-17 00:43:15 -0300

    refactor: remover rotas de etapas

--- src/routes/client.tsx
+++ src/routes/client.tsx
@@ -2,7 +2,6 @@ import type { RouteObject } from 'react-router-dom'
 
 import ClientHome from '@/pages/client/home'
 import ClientRegistrarAvarias from '@/pages/client/registrar-avarias'
-import ClientRegistrarAvariasEtapa001 from '@/pages/client/registrar-avarias/etapa-001'
 
 export const clientRoutes: RouteObject[] = [
   {
@@ -12,9 +11,5 @@ export const clientRoutes: RouteObject[] = [
   {
     path: 'registrar-avarias',
     element: <ClientRegistrarAvarias />,
-  },
-  {
-    path: 'registrar-avarias/etapa-001',
-    element: <ClientRegistrarAvariasEtapa001 />
   },
 ]
