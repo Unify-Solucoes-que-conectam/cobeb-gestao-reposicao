567453c48e81b9dae8d1c64276f6dbeb665bdd5d
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-07-19 14:56:26 -0300

    hotfix: redirecionamento indesejado no PWA

--- src/pages/mobile-layout.tsx
+++ src/pages/mobile-layout.tsx
@@ -1,22 +1,9 @@
-import { Outlet, useNavigate } from 'react-router'
+import { Outlet } from 'react-router';
 
-import { AppHeader } from '@/components/custom/mobile/app-header'
-import { HeaderProvider } from '@/providers/mobile/HeaderProvider'
-import { useEffect } from 'react';
+import { AppHeader } from '@/components/custom/mobile/app-header';
+import { HeaderProvider } from '@/providers/mobile/HeaderProvider';
 
 export default function MobileLayout() {
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // Verifica se o app está rodando dentro da janela do PWA instalado
-    const isPWA = window.matchMedia('(display-mode: standalone)').matches;
-
-    if (isPWA) {
-      // Se já estiver instalado e abrindo o PWA, manda direto para o login
-      navigate('/auth/login');
-    }
-  }, [navigate]);
 
   return (
     <HeaderProvider>
